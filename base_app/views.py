3476bb97b4af097a5bc99ac5280b254441e65726
gLiNtAlpHa <sesaalolu@gmail.com>
2023-01-04 21:35:31 +0100

    template file  created

--- base_app/views.py
+++ base_app/views.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Task
+
+
 
 # Register your models here.
-def tasklist(request):
-    return HttpResponse('to do list')
+class TaskList(ListView):
+    model = Task
