2970a475a4c5399b9dae264706c394bfae7eeebe
gLiNtAlpHa <sesaalolu@gmail.com>
2023-01-05 18:37:44 +0100

    created task form view

--- base_app/views.py
+++ base_app/views.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Task
 
 
@@ -16,3 +17,6 @@ class TaskList(ListView):
 class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
+    template_name = "base_app/task.html"
+class TaskCreate(CreateView):
+
