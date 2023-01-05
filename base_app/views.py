9feaa677f877a2547752425dd9d80cae63bf078c
gLiNtAlpHa <sesaalolu@gmail.com>
2023-01-05 14:50:26 +0100

    created list detail

--- base_app/views.py
+++ base_app/views.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 # from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
 
 
@@ -9,3 +10,9 @@ from .models import Task
 # Register your models here.
 class TaskList(ListView):
     model = Task
+    context_object_name = 'tasks'
+
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
