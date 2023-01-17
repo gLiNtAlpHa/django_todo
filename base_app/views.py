14b8245096a7336f8e2daceb94f2a204c67b5edf
gLiNtAlpHa <sesaalolu@gmail.com>
2023-01-17 12:35:32 +0100

    login view created

--- base_app/views.py
+++ base_app/views.py
@@ -6,10 +6,17 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Task
-
+from django.contrib.auth.views import LoginView
 
 
 # Register your models here.
+class CustomLoginView(LoginView):
+    template_name = 'base_app/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
@@ -19,16 +26,22 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = "base_app/task.html"
+
+
 class TaskCreate(CreateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
+
 class TaskUpdate(UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
-class TaskDelete(DeleteView):
+
+
+class DeleteView(DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
+
