f9a78668d4bfd2bf55de66346605095dff2cedd6
gLiNtAlpHa <sesaalolu@gmail.com>
2023-01-18 16:09:58 +0100

    created a register page

--- base_app/views.py
+++ base_app/views.py
@@ -3,11 +3,13 @@ from django.contrib import admin
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 from .models import Task
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 
 # Register your models here.
@@ -18,6 +20,20 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('tasks')
+
+
+class RegisterPage(FormView):
+    template_name = 'base_app/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+
+        return super(RegisterPage, self).form_valid(form)
 
 
 class TaskList(LoginRequiredMixin, ListView):
@@ -45,21 +61,17 @@ class TaskDetail(LoginRequiredMixin, DetailView):
 
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = ['tittle','description','complete']
+    fields = ['tittle', 'description', 'complete']
     success_url = reverse_lazy('tasks')
-
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(TaskCreate, self).form_valid(form)
 
 
-
-
-
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = ['tittle','description','complete']
+    fields = ['tittle', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
 
