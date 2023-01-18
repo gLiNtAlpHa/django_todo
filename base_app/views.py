9a191e970c76649a9601699a801c2af76eba4a32
gLiNtAlpHa <sesaalolu@gmail.com>
2023-01-18 16:36:34 +0100

    replace tittle__icontains to tittle__startswith for d filtering of search table

--- base_app/views.py
+++ base_app/views.py
@@ -58,7 +58,7 @@ class TaskList(LoginRequiredMixin, ListView):
 
         search_input = self.request.GET.get('search-area') or ''
         if search_input:
-            context['tasks'] = context['tasks'].filter(tittle__icontains=search_input)
+            context['tasks'] = context['tasks'].filter(tittle__startswith=search_input)
 
         context['search_input'] = search_input
         return context
